c6b07632a44b1967ab781cb9c22461685fe57e83
Yashodha8890 <yashodha23000@student.hamk.fi>
2024-01-26 12:23:24 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        echo"Hello world";
+
+        //if we want use html tags inside the php code can use this way;
+
+        echo "<h1>Hello world</h1>";    
+    ?>
+
+
+</body>
+</html>
+
